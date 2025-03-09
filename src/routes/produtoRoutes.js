5f7a6ded2a7ddc46cc1aa92d790a16bd613ad278
Laís Lima <143414398+lis-lais@users.noreply.github.com>
2025-03-09 17:58:52 -0300

    Update produtoRoutes.js

--- src/routes/produtoRoutes.js
+++ src/routes/produtoRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const produtoController = require('./controllers/produtoController');
+const produtoController = require('../controllers/produtoController');
 
 router.post('/', produtoController.createProduto);
 router.get('/', produtoController.findAllProdutos);
